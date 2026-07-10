310c185b0eec35aff5a49ba93b7217310a6e15b8
jfilhoempresarial-ops <jfilhoempresarial@gmail.com>
2026-07-10 20:00:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-
+const CACHE = 'tr-alienigena-v1';
+const URLS = ['/', '/index.html', '/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
